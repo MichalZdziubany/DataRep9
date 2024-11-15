00f669400b64dc4d1f37ca628bcd62a0c8b32bde
Michal Zdziubany <G00419292@atu.ie>
2024-11-15 10:39:31 +0000

    server displays by id correctly

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.get('/api/movies', async (req, res) => {
 });
 
 //adds a parameter for object id in the url which returns a specific movie from the database
-app.get('/api/movie/:id', async (req, res) => {
+app.get('/api/movies/:id', async (req, res) => {
   const movie = await Movie.findById(req.params.id);
   res.json(movie);
 });
