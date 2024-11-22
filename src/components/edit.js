dbb0a81bfdf5a2fc04e933061d0e21b50ae5aee8
Michal Zdziubany <G00419292@atu.ie>
2024-11-22 09:45:28 +0000

    edit file created and cloned from repo

---
+++ src/components/edit.js
@@ -0,0 +1,71 @@
+//add all imports
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+export default function Edit(props) {
+//defines parameters
+  let { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [year, setYear] = useState("");
+  const [poster, setPoster] = useState("");
+  const navigate = useNavigate();
+
+//edits the movie to what the user inputs
+useEffect(() => {
+    axios.get('http://localhost:4000/api/movie/' + id)
+        .then((response) => {
+            setTitle(response.data.title);
+            setYear(response.data.year);
+            setPoster(response.data.poster);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}, [id]);
+
+//posts the edited movie to the database
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMovie = { id, title, year, poster };
+    axios.put('http://localhost:4000/api/movie/' + id, newMovie)
+        .then((res) => {
+            console.log(res.data);
+            navigate('/read');
+        });
+}
+
+{/*form to edit movie and sent to database*/}
+return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Edit Movie Title: </label>
+                <input type="text" 
+                className="form-control" 
+                value={title} 
+                onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <div className="form-group">
+                <label>Edit Release Year: </label>
+                <input type="text" 
+                className="form-control" 
+                value={year} 
+                onChange={(e) => setYear(e.target.value)} />
+            </div>
+            <div className="form-group">
+                <label>Edit Poster URL: </label>
+                <input type="text" 
+                className="form-control" 
+                value={poster} 
+                onChange={(e) => setPoster(e.target.value)} />
+            </div>
+            <div className="form-group">
+                <input type="submit" value="Edit Movie" className="btn btn-primary" />
+            </div>
+        </form>
+    </div>
+);
+}
